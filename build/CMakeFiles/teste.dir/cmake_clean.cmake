dbe2f9c382a07721f41e6d143f3073cbf38f7285
dan01542 <danielschmitzsc@gmail.com>
2024-03-17 21:41:59 -0300

    Spaceship first implementation

---
+++ build/CMakeFiles/teste.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/teste.dir/teste.c.o"
+  "CMakeFiles/teste.dir/teste.c.o.d"
+  "teste"
+  "teste.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/teste.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- src/main.c
+++ src/main.c
@@ -5,6 +5,19 @@
 #include "blast.h"
 #include "asteroid.h"
 
+void drawShip(){
+    ALLEGRO_TRANSFORM transform;
+    al_identity_transform(&transform);
+    al_rotate_transform(&transform, 21);
+    al_translate_transform(&transform, 56, 23);
+    al_use_transform(&transform);
+    al_draw_line(-8,9, 0, -11, al_map_rgb(0, 254, 0), 3.0f);
+    al_draw_line(0,-11, 8, 9, al_map_rgb(0, 254, 0), 3.0f);
+    al_draw_line(-6,4, -1, 4, al_map_rgb(0, 254, 0), 3.0f);
+    al_draw_line(6,4, 1, 4, al_map_rgb(0, 254, 0), 3.0f);
+
+}
+
 int main (){
 
     ALLEGRO_DISPLAY *display;
@@ -14,7 +27,7 @@ int main (){
     al_init();
     al_init_primitives_addon();
 
-    display = al_create_display(1200, 600);
+    display = al_create_display(600, 600);
     queue = al_create_event_queue();
     timer = al_create_timer( 1.0 / 60 );
     
@@ -23,37 +36,29 @@ int main (){
     al_register_event_source(queue, al_get_keyboard_event_source()); //Handles events from keyboard
     al_register_event_source(queue, al_get_display_event_source(display)); //Handles events from display
     al_register_event_source(queue, al_get_timer_event_source(timer));
-
+    
     //Main game loop
     int running = 1;
+
+    al_start_timer(timer);
     while(running){
-        
 
         ALLEGRO_EVENT event;
         al_wait_for_event(queue, &event);
-
         if(event.type == ALLEGRO_EVENT_KEY_UP || event.type == ALLEGRO_EVENT_DISPLAY_CLOSE){
             running = 0;
         }
 
         if(event.type == ALLEGRO_EVENT_TIMER){
+            drawShip();
             al_flip_display();
         }
+        
     }
 
     al_destroy_display(display);
     al_uninstall_keyboard();
+    al_destroy_timer(timer);
 
     return 0;
-/* 
-    spaceship s = {0.0, 0.0, 2.0, 2.0, 0, {0.0, 255.0, 0, 0}};
-    spaceship *Ps = &s;
-
-
-    al_draw_line(-8, 9, 0, -11, Ps -> color, 3.0f);
-    al_draw_line(0, -11, 8, 9, Ps -> color, 3.0f);
-    al_draw_line(-6, 4, -1, 4, Ps -> color, 3.0f);
-    al_draw_line(6, 4, 1, 4, Ps -> color, 3.0f);
-
- */
 }
