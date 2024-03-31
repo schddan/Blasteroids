7ef5533efd5c76c5bb28fa967ddfb53026991c90
dan01542 <danielschmitzsc@gmail.com>
2024-03-31 18:41:26 -0300

    Asteroid Repositioning

--- src/asteroid.c
+++ src/asteroid.c
@@ -10,6 +10,34 @@ float RandomFloat(float min, float max){
    return ((max - min) * ((float)rand() / RAND_MAX)) + min;
 }
 
+void definePositionAndHeading(asteroid * asteroid){
+    int quadrant = rand() % 4 + 1; //random number beetween 1 to 4
+    switch (quadrant)
+    {
+    case 1:
+        asteroid->sx = rand() % DISPLAY_WIDTH;
+        asteroid->sy = -40;
+        asteroid->heading = RandomFloat(-1.5, 1.5);
+        break;
+    case 2:
+        asteroid->sx = DISPLAY_WIDTH;
+        asteroid->sy = rand() % DISPLAY_HEIGHT;
+        asteroid->heading = RandomFloat(-3.1, 0);
+        break;
+    case 3:
+        asteroid->sx = rand() % DISPLAY_WIDTH;
+        asteroid->sy = DISPLAY_HEIGHT;
+        asteroid->heading = RandomFloat(1.6, 4.7);
+        break;
+    case 4:
+        asteroid->sx = 0;
+        asteroid->sy = rand() % DISPLAY_HEIGHT;
+        asteroid->heading = RandomFloat(0, 3.1);
+        break;
+    }
+}
+
+
 void drawAsteroid(asteroid * asteroid){
     ALLEGRO_TRANSFORM transform;
     al_identity_transform(&transform);
@@ -30,34 +58,14 @@ void drawAsteroid(asteroid * asteroid){
     al_draw_line(0,15, -20, 20, asteroid->color, 3.0f);
     asteroid->sx += asteroid->speed * sin(asteroid->heading);
     asteroid->sy+= asteroid->speed * cos(asteroid->heading);
+    if(asteroid->sx > DISPLAY_WIDTH || asteroid->sx < 0 || asteroid->sy > DISPLAY_HEIGHT || asteroid -> sy < 0){
+        definePositionAndHeading(asteroid);
+    }
 }
 
 asteroid createAsteroid(ALLEGRO_COLOR color){
     asteroid asteroid;
-    int quadrant = rand() % 4 + 1; //random number beetween 1 to 4
-    switch (quadrant)
-    {
-    case 1:
-        asteroid.sx = rand() % DISPLAY_WIDTH;
-        asteroid.sy = -40;
-        asteroid.heading = RandomFloat(-1.5, 1.5);
-        break;
-    case 2:
-        asteroid.sx = DISPLAY_WIDTH;
-        asteroid.sy = rand() % DISPLAY_HEIGHT;
-        asteroid.heading = RandomFloat(-3.1, 0);
-        break;
-    case 3:
-        asteroid.sx = rand() % DISPLAY_WIDTH;
-        asteroid.sy = DISPLAY_HEIGHT;
-        asteroid.heading = RandomFloat(1.6, 4.7);
-        break;
-    case 4:
-        asteroid.sx = 0;
-        asteroid.sy = rand() % DISPLAY_HEIGHT;
-        asteroid.heading = RandomFloat(0, 3.1);
-        break;
-    }
+    definePositionAndHeading(&asteroid);
     asteroid.rotationDirection = 0;
     asteroid.speed = 1;
     asteroid.rotVelocity = 0.07;
